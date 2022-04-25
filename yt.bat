2f92248bcc2b965d795ba2990e4feb96b51a05c6
HamingnottT <Jnottingham836@yahoo.com>
2022-04-25 08:38:54 -0500

    modified program batch file

--- yt.bat
+++ yt.bat
@@ -1,5 +1,13 @@
+::start
+
 @echo off
 
-python -m src.main
+%~d0			::calls the currect drive letter
+
+cd "%~dp0"		::searches for path of batch file
+
+python -m src.main	::calls main.py from src
 
 timeout.exe 1
+
+::end
